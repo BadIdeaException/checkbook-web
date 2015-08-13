ccc88734f54b6d957e35b24a5537c2e2b1db5ba9
chris <tigerman33@gmx.net>
2015-08-13 15:34:46 +0200

    Added $resolved property to stay close to the $resource API

--- src/js/model/Category.js
+++ src/js/model/Category.js
@@ -6,6 +6,10 @@ module.factory('Category', [ '$http', function($http) {
 	var Category = function(values) {
 		var self = this;
 
+		// No server interaction has happened yet
+		// See https://docs.angularjs.org/api/ngResource/service/$resource
+		self.$resolved = false;
+
 		// Copy provided data values into self
 		Object.keys(values).forEach(function(key) {
 			self[key] = values[key];
@@ -15,13 +19,18 @@ module.factory('Category', [ '$http', function($http) {
 	Category.prototype.$get = function() {
 		var self = this;
 
-		var uri = URI_TEMPLATE.fill(self);
+		var uri = URI_TEMPLATE.fillFromObject(self);
 		self.$promise = $http
 			.get(uri)
 			.success(function(data) {
 				for (key in data) 
 					self[key] = data[key];
+				return self;
 			})
+			// .finally(function(x) {
+			// 	self.$resolved = true;
+			// 	return x;
+			// })
 
 		return self;
 	}
--- test/model/Category.spec.js
+++ test/model/Category.spec.js
@@ -14,7 +14,8 @@ describe('Category', function() {
 	it('should use the provided data values when constructing a new instance', function() {
 		var category = new Category(CATEGORY);
 
-		for (var key in CATEGORY) 
+		expect(category).to.have.property('$resolved', false);
+		for (var key in CATEGORY)
 			expect(category).to.have.property(key, CATEGORY[key]);
 	});
 
@@ -27,13 +28,22 @@ describe('Category', function() {
 
 		var category = new Category(CATEGORY);
 		category = category.$get();
+		$httpBackend.flush();
+		var spy = sinon.spy();
+		category.$promise.then(spy);
+		expect(spy).to.have.been.called;
 
 		// Check that promise is set correctly
 		expect(category).to.have.property('$promise');
 		expect(category.$promise).to.respondTo('then');
+		// $resolved should be true after server interaction
+		expect(category).to.have.property('$resolved', true);
 
 		// Check that the new values are present
 		for (var key in RESPONSE) 
 			expect(category).to.have.property(key, RESPONSE[key]);
-	})
+
+	});
+
+
 })
