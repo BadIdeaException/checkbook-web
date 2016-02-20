e0768965033212803c58db75487ea0098a5cbdde
chris <tigerman33@gmx.net>
2016-02-20 17:55:25 +0100

    Updated some versions

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'sinon-chai', 'chai-as-promised', 'chai'],
+    frameworks: ['mocha', 'chai-as-promised', 'sinon-chai', 'chai'],
 
 
     // list of files / patterns to load in the browser
