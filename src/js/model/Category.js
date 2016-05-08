b1ee37f66d5fe0ee7609dada68aa647ca40e740f
chris <tigerman33@gmx.net>
2016-05-08 11:46:15 +0200

    Fixed typo in dependency declaration (missing r on resource)

--- src/js/model/Category.js
+++ src/js/model/Category.js
@@ -1,6 +1,6 @@
 var module = angular.module('Checkbook.Model', [ 'ngResource' ]);
 
-module.factory('Category', [ '$resouce', function($resource) {
+module.factory('Category', [ '$resource', function($resource) {
 	var Category = $resource('/categories/:id', { 'id': '@id' });
 
 	return Category;
