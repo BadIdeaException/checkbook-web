05e57457c27616a1caaa1441b9f94814b7294f4a
chris <tigerman33@gmx.net>
2016-05-17 08:57:08 +0200

    Added fetchEntries and tests

--- src/js/model/CategoryForMonth.js
+++ src/js/model/CategoryForMonth.js
@@ -1,90 +1,128 @@
 var module = angular.module('Checkbook.Model');
 
-module.factory('CategoryForMonth', [ '$http', 'Entry', function($http, Entry) {
-	const URI_TEMPLATE = new URITemplate('/months/{monthid}/categories/{id}');
+module.factory('CategoryForMonth', [ '$resource', '$q', 'Entry', 'Category', function($resource, $q, Entry, Category) {
 
-	// FOLLOWING HAS BEEN COPIED AND PASTED FROM CATEGORY OBJECT
-	var CategoryForMonth = function(values) {
+	var _CategoryForMonth = $resource('/months/:monthid/categories/:id',
+			{ id: '@id' });
+
+	function CategoryForMonth() {
 		var self = this;
 
-		// No server interaction has happened yet
-		// See https://docs.angularjs.org/api/ngResource/service/$resource
-		self.$resolved = false;
+		// Call through to Category and $resource-created constructors
+		Category.apply(self, arguments);
+		_CategoryForMonth.apply(self, arguments);		
+	}
+	// Copy static methods from $resource-created constructor
+	angular.merge(CategoryForMonth, _CategoryForMonth);
+	CategoryForMonth.prototype = _CategoryForMonth.prototype;
 
-		// Copy provided data values into self
-		Object.keys(values).forEach(function(key) {
-			self[key] = values[key];
-		});
+	// Delete "write" actions (both static and instance versions)
+	delete CategoryForMonth.save;
+	delete CategoryForMonth.delete;
+	delete CategoryForMonth.remove;
+
+	delete CategoryForMonth.prototype.$save;
+	delete CategoryForMonth.prototype.$delete;
+	delete CategoryForMonth.prototype.$remove;
+
+	/**
+	 * Fetches the entries belonging to this category and month. If entries are already loaded, i.e. if 
+	 * `entries` exists, a promise resolved with the value of `entries` is returned. Otherwise,
+	 * `Entry.querySpecific` is called, the result assigned to `entries` and the contained $promise field returned.
+	 * @returns {Promise} A promise resolving to the entries
+	 */
+	CategoryForMonth.prototype.fetchEntries = function() {
+		if (!this.entries) {
+			this.entries = Entry.querySpecific({ monthid: this.monthid, category: this.id });
+			return this.entries.$promise;
+		} else 
+			return $q.when(this.entries);		
 	};
+
+	// const URI_TEMPLATE = new URITemplate('/months/{monthid}/categories/{id}');
+
+	// // FOLLOWING HAS BEEN COPIED AND PASTED FROM CATEGORY OBJECT
+	// var CategoryForMonth = function(values) {
+	// 	var self = this;
+
+	// 	// No server interaction has happened yet
+	// 	// See https://docs.angularjs.org/api/ngResource/service/$resource
+	// 	self.$resolved = false;
+
+	// 	// Copy provided data values into self
+	// 	Object.keys(values).forEach(function(key) {
+	// 		self[key] = values[key];
+	// 	});
+	// };
 	
-	CategoryForMonth.query = function(monthid) {
-		var uri = URI_TEMPLATE.expand({ monthid: monthid }); // Strip id param from URI
-		if (uri[uri.length - 1] === '/') uri = uri.substr(0, uri.length - 1) // Strip trailing slash
+	// CategoryForMonth.query = function(monthid) {
+	// 	var uri = URI_TEMPLATE.expand({ monthid: monthid }); // Strip id param from URI
+	// 	if (uri[uri.length - 1] === '/') uri = uri.substr(0, uri.length - 1) // Strip trailing slash
 
-		var categories = [];
+	// 	var categories = [];
 
-		function transformResponse(data) {
-				return data.map(function createCategoryForMonth(item) {
-					// Turn data items into CategoryForMonth objects
-					var category = new CategoryForMonth(item);
-					category.$resolved = true; // The new categories have all been loaded from the server
-					return category;
-				});			
-		};
+	// 	function transformResponse(data) {
+	// 			return data.map(function createCategoryForMonth(item) {
+	// 				// Turn data items into CategoryForMonth objects
+	// 				var category = new CategoryForMonth(item);
+	// 				category.$resolved = true; // The new categories have all been loaded from the server
+	// 				return category;
+	// 			});			
+	// 	};
 
-		categories.$promise = $http
-			.get(uri, { transformResponse: transformResponse })
-			.success(function(data) {
-				// Fill result array with loaded values
-				data.forEach(function(item) { categories.push(item); });
-			});
+	// 	categories.$promise = $http
+	// 		.get(uri, { transformResponse: transformResponse })
+	// 		.success(function(data) {
+	// 			// Fill result array with loaded values
+	// 			data.forEach(function(item) { categories.push(item); });
+	// 		});
 
-		return categories;
-	};
+	// 	return categories;
+	// };
 
-	CategoryForMonth.prototype.$get = function() {
-		var self = this;
+	// CategoryForMonth.prototype.$get = function() {
+	// 	var self = this;
 
-		var uri = URI_TEMPLATE.expand(self);
-		self.$promise = $http
-			.get(uri)
-			.then(function success(response) {
-				var data = response.data;
-				// Copy new values into self
-				for (key in data) 
-					self[key] = data[key];
-				return self;
-			})
-			.finally(function(x) {
-				// Set resolved to true because server interaction has happened
-				self.$resolved = true;
-				return x;
-			});
+	// 	var uri = URI_TEMPLATE.expand(self);
+	// 	self.$promise = $http
+	// 		.get(uri)
+	// 		.then(function success(response) {
+	// 			var data = response.data;
+	// 			// Copy new values into self
+	// 			for (key in data) 
+	// 				self[key] = data[key];
+	// 			return self;
+	// 		})
+	// 		.finally(function(x) {
+	// 			// Set resolved to true because server interaction has happened
+	// 			self.$resolved = true;
+	// 			return x;
+	// 		});
 
-		return self;
-	};
+	// 	return self;
+	// };
 
-	// END OF COPIED CONTENT
+	// // END OF COPIED CONTENT
 
-	CategoryForMonth.prototype.getEntries = function() {
-		var self = this;
-		if (self.entries === undefined || self.entries === null) 
-			self.entries = Entry.query(self.monthid, self.id);
+	// CategoryForMonth.prototype.getEntries = function() {
+	// 	var self = this;
+	// 	if (self.entries === undefined || self.entries === null) 
+	// 		self.entries = Entry.query(self.monthid, self.id);
 
-		return self.entries;
-	};
+	// 	return self.entries;
+	// };
 
-	CategoryForMonth.prototype.getTotal = function() {
-		var self = this;
-		// Return computed value if entries are present and have finished loading
-		if (self.entries && self.entries.$resolved)
-			return self.entries.reduce(function(total, entry) {
-				return total + entry.value;
-			}, 0);
-		// Otherwise, return own value
-		else
-			return self.value;
-	};
+	// CategoryForMonth.prototype.getTotal = function() {
+	// 	var self = this;
+	// 	// Return computed value if entries are present and have finished loading
+	// 	if (self.entries && self.entries.$resolved)
+	// 		return self.entries.reduce(function(total, entry) {
+	// 			return total + entry.value;
+	// 		}, 0);
+	// 	// Otherwise, return own value
+	// 	else
+	// 		return self.value;
+	// };
 
 	return CategoryForMonth;
 }]);
--- test/model/CategoryForMonth.spec.js
+++ test/model/CategoryForMonth.spec.js
@@ -13,122 +13,59 @@ describe('CategoryForMonth', function() {
 		$httpBackend = $injector.get('$httpBackend');
 	}));
 
-	it('should use the provided data values when constructing a new instance', function() {
-		var category = new CategoryForMonth(CATEGORY);
-
-		expect(category).to.have.property('$resolved', false);
-		// All values passed to the constructor should be present
-		for (var key in CATEGORY)
-			expect(category).to.have.property(key, CATEGORY[key]);
+	it('should only have "reading" an no "writing" actions', function() {
+		expect(CategoryForMonth).itself.to.respondTo('query');
+		expect(CategoryForMonth).itself.to.respondTo('get');
+		expect(CategoryForMonth).itself.to.not.respondTo('save');
+		expect(CategoryForMonth).itself.to.not.respondTo('delete');
+		expect(CategoryForMonth).itself.to.not.respondTo('remove');
+		
+		var c = new CategoryForMonth();
+		expect(c).to.respondTo('$query');
+		expect(c).to.respondTo('$get');
+		expect(c).to.not.respondTo('$save');
+		expect(c).to.not.respondTo('$delete');
+		expect(c).to.not.respondTo('$remove');
 	});
 
-	it('should get an array of categories when calling .query', function() {
-		const RESPONSE = [ CATEGORY, { id: 2, caption: '2', monthid: 0, value: 200 }];
-		$httpBackend
-			.expectGET('/months/0/categories')
-			.respond(200, RESPONSE);
+	describe('.fetchEntries', function() {
+		it('should hit the server when entries are not loaded', function() {
+			var category = new CategoryForMonth(CATEGORY);
+			var entryResponse = { id: 25 }; // Arbitrary response, don't need a full entry here
 
-		var categories = CategoryForMonth.query(0);
-		$httpBackend.flush();
+			$httpBackend
+				.expectGET('/months/' + category.monthid + '/categories/' + category.id + '/entries')
+				.respond(200, [ entryResponse ]);
 
-		// $http promise should be set on the result
-		expect(categories).to.have.property('$promise');
-		expect(categories.$promise).to.respondTo('then');
+			var pResult;
 
-		// Result should have the right length
-		expect(categories).to.have.length(RESPONSE.length);
-		for (var i = 0; i < categories.length; i++) {			
-			var category = categories[i];
-			// It should be a Category
-			expect(category).to.be.an.instanceOf(CategoryForMonth);
-			// Its $resolved property should be true
-			expect(category).to.have.property('$resolved', true);
-			// All values should be set correctly
-			for (var key in RESPONSE[i])
-				expect(category).to.have.property(key, RESPONSE[i][key]);
-		}
-	});
+			var p = category
+				.fetchEntries()
+				.then(function(r) { pResult = r; });
 
-	it('should update itself when calling .$get', function() {
-		const RESPONSE = { id: CATEGORY.id, caption: CATEGORY.caption + CATEGORY.caption, monthid: CATEGORY.monthid, value: CATEGORY.value + 1 };
+			$httpBackend.flush();
 
-		$httpBackend
-			.expectGET('/months/' + CATEGORY.monthid + '/categories/' + CATEGORY.id)
-			.respond(200, RESPONSE);
+			expect(pResult).to.exist.and.have.length(1);
+			expect(pResult[0]).to.have.property('id', entryResponse.id);
+		});
 
-		var category = new CategoryForMonth(CATEGORY);
-		category = category.$get();
-		$httpBackend.flush();
+		it('should not hit the server when entries are already loaded', inject(function($rootScope) {
+			var category = new CategoryForMonth(CATEGORY);
+			category.entries = [ { id: 25 } ]; // Don't need a full entry here
 
-		// Check that promise is set correctly
-		expect(category).to.have.property('$promise');
-		expect(category.$promise).to.respondTo('then');
-		// $resolved should be true after server interaction
-		expect(category).to.have.property('$resolved', true);
+			$httpBackend
+				.whenGET(function() { return true; }) // Match any url
+				.respond(function() { expect.fail('Hit the server when it shouldn\'t have'); });
 
-		// Check that the new values are present
-		for (var key in RESPONSE) 
-			expect(category).to.have.property(key, RESPONSE[key]);
-	});
+			var pResult;
+			var p = category
+				.fetchEntries()
+				.then(function(r) { pResult = r; });
 
-	it('should load entries when calling getEntries for the first time', function() {
-		var query = sinon.spy(Entry, 'query');
+			$rootScope.$apply(); // Need to apply manually since $httpBackend.flush isn't doing it for us
 
-		var category = new CategoryForMonth(CATEGORY);
-		var entries = category.getEntries();
-
-		expect(query).to.have.been.called;
-		var args = query.getCall(0).args;
-
-		// There are two different ways this could have been called
-		if (args.length === 2)
-			expect(args).to.deep.equal([ CATEGORY.monthid, CATEGORY.id ]);
-		else {
-			args = args[0];
-			expect(args).to.have.property('monthid', CATEGORY.monthid);
-			expect(args).to.have.property('category', CATEGORY.id);
-		}
-
-		query.restore();
-	});
-
-	it('should use cached entries when calling getEntries subsequently', function() {
-		var category = new CategoryForMonth(CATEGORY);
-
-		const ENTRIES = [{ id: 1, caption: '1', category: CATEGORY.id, datetime: new Date(0), value: CATEGORY.value, details: 'details'}];
-		ENTRIES.$resolved = true;
-		category.entries = ENTRIES;
-
-		expect(category.getEntries()).to.equal(ENTRIES);
-	});
-
-	it('should use own value when calling getTotal and entries haven\'t been loaded', function() {
-		var category = new CategoryForMonth(CATEGORY);
-
-		// Case 1: entries haven't been loaded yet
-		expect(category.getTotal()).to.equal(category.value);
-
-		// Case 2: loading is in progress but hasn't completed
-		const ENTRIES = [{ id: 1, caption: '1', category: CATEGORY.id, datetime: new Date(0), value: CATEGORY.value + 1, details: 'details'}];
-		category.entries = ENTRIES;
-		category.entries.$resolved = false;
-		expect(category.getTotal()).to.equal(category.value);		
-	});
-
-	it('should compute total value from entries if they are available', function() {
-		var category = new CategoryForMonth(CATEGORY);
-
-		const ENTRIES = [
-			{ id: 1, caption: '1', category: CATEGORY.id, datetime: new Date(0), value: CATEGORY.value + 1, details: '1'},
-			{ id: 2, caption: '2', category: CATEGORY.id, datetime: new Date(0), value: CATEGORY.value + 2, details: '2' }
-		];
-
-		category.entries = ENTRIES.map(function(entry) { return new Entry(entry); });
-		category.entries.$resolved = true;
-
-		// Expect computed values to be the sum of the entries' values
-		expect(category.getTotal()).to.equal(ENTRIES.reduce(function(total, entry) {
-			return total + entry.value;
-		}, 0));
+			expect(pResult).to.exist.and.have.length(1);
+			expect(pResult[0]).to.deep.equal(category.entries[0]);
+		}));
 	});
 });
