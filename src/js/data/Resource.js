d6dd18fecde9f82f06c5949266542e4a85286175
chris <tigerman33@gmx.net>
2016-05-18 18:49:03 +0200

    Added functionality to make resources read-only by passing a readOnly flag in options. Also caching can be turned on in options by setting options.cache to either true (in which case on will be created using the resource url as the cache name) or a Cache object to use Note that actual caching isn't implemented yet, this is just prep work.

--- src/js/data/Resource.js
+++ src/js/data/Resource.js
@@ -3,10 +3,13 @@
 /**
  * Decorate angular's $resource factory to facilitate caching and the inclusion of an 'update' action (HTTP PUT as per REST specification)
  */
+(function() { // IIFE to keep this variable from polluting the global scope
+var resourceProviderDefaultActions;
+
 angular
 	.module('Checkbook.Data')
 	.config([ '$resourceProvider' , function($resourceProvider) {
-		$resourceProvider.defaults.actions = { 
+		resourceProviderDefaultActions = $resourceProvider.defaults.actions = { 
 			'get': { method: 'GET' },
 			'create': { method: 'POST' },
 			'update': { method: 'PUT' },
@@ -15,22 +18,71 @@ angular
 			'delete': { method: 'DELETE' } 
 		};
 	}])
-	.decorator('$resource', [ '$delegate', '$cacheFactory', function($delegate, $cacheFactory) {
-		// var cache = $cacheFactory('resources');
-		
-		var decorated = function() {			
-			var Resource = $delegate.apply(this, arguments);
+	.decorator('$resource', [ '$delegate', '$cacheFactory', '$log', function($delegate, $cacheFactory, $log) {
+		/**
+		 * Removes all methods (both static and instance versions) from `Resource` that conform to actions using
+		 * methods other than GET or HEAD.
+		 * @param  {Resource} Resource The Resource constructor to work on
+		 * @param  {Object} actions  The extra actions that were given to the $resource call		 
+		 */
+		function removeWriteActions(Resource, actions) {
+			var actions = angular.extend({}, actions, resourceProviderDefaultActions);
+			Object
+				.keys(actions)
+				.filter(function(actionName) { return actions[actionName].method.toUpperCase() !== 'GET' && actions[actionName].method.toUpperCase() !== 'HEAD'; })
+				.forEach(function(actionName) { delete Resource[actionName]; delete Resource.prototype['$' + actionName]; });
+		}
 
-			Resource.prototype.$save = function(params, success, error) {
+		/**
+		 * Decorated version of the $resource factory function.
+		 * @param  {[type]} url           [description]
+		 * @param  {[type]} paramDefaults [description]
+		 * @param  {[type]} actions       [description]
+		 * @param  {Object} options       In addition to the options understood by the delegate version, the following are available:
+		 *    `readOnly`: If `true`, the resource will only have actions with method GET or HEAD
+		 *    `cache`: If `true`, a cache will be created using `url` as cache name or an existing one of that name retrieved. If a `Cache` object, it will be used.
+		 * @return {[type]}               [description]
+		 */
+		var decorated = function(url, paramDefaults, actions, options) {			
+			// Make sure parameters are defined objects and make copies so we can write and delete in them
+			actions = angular.extend({}, actions);
+			options = angular.extend({}, options);
+			// Copy over those options introduced by us over into a new object, then remove them from the
+			// original options parameter (because I think it will trip up the original $resource when populating
+			// underlying $http config objects)
+			var newOptions = {};
+			[ 'readOnly', 'cache' ].forEach(function(key) { newOptions[key] = options[key]; delete options[key]; });			
+						
+			
+
+			// Call through to the delegate factory
+			var Resource = $delegate.apply(this, arguments);			
+			// If the resource is supposed to be read-only, remove all methods that are not GET or HEAD actions
+			if (newOptions.readOnly)
+				removeWriteActions(Resource, actions);
+
+			
+
+			if (newOptions.cache === true)
+				Resource.cache = $cacheFactory.get(url) || $cacheFactory(url); // Create or retrieve cache
+			else if (angular.isObject(newOptions.cache))
+				Resource.cache = newOptions.cache;
+			
+
+
+			// This instance method will not be present if newOptions.readOnly flag is set
+			Resource.prototype.$save = !newOptions.readOnly && function(params, success, error) {
 				return Resource.save(params, this, success, error);
 			};
 
 			/**
 			 * Convenience method that executes the update action if an `id` field is defined on the `data`,
 			 * and the create action if it isn't.
+			 *
+			 * This method and its instance version `$save` will not be present in resources created with the `options.readOnly` flag set 
 			 */
-			Resource.save = function(params, data, success, error) {
-				// If the second parameter is not an object, then params MUST have been omitted,
+			Resource.save = !newOptions.readOnly && function(params, data, success, error) {
+				// If the second parameter is not an object, then params must have been omitted,
 				// because the two following ones, if present, are functions
 				// So reassign parameters to account for this (although some of them might be undefined anyway)
 				if (!angular.isObject(data)) {
@@ -41,8 +93,29 @@ angular
 				else
 					return Resource.create(arguments);
 			}
+
+			
+
+			// Cache-aware version of the delegate method
+			var _get = Resource.get;
+			Resource.get = function() {
+				var self = this;
+
+				var result = _get.apply(this, arguments);
+				result.$promise.then(function cacheResult(resource) {
+					if (resource.id || resource.id === 0)
+						self.cache.put(resource.id, resource);
+					else
+						$log.warn('Resource does not have an id - did not cache');
+
+					return resource;
+				});
+				return result;
+			};
+
 			return Resource;
 		};	
 
 		return decorated;
 	}]);
+})();
--- test/data/Resource.spec.js
+++ test/data/Resource.spec.js
@@ -9,14 +9,15 @@ describe('Resource', function() {
 		$httpBackend = _$httpBackend_;
 	}));
 
-	it('should have get, query, create, update, delete and remove actions', function() {
-		var Resource = $resource();
+	it('should have get, query, create, update, delete and remove actions and a save method per default', function() {
+		var Resource = $resource('/');
 		expect(Resource).itself.to.respondTo('get');
 		expect(Resource).itself.to.respondTo('query');
 		expect(Resource).itself.to.respondTo('create');
 		expect(Resource).itself.to.respondTo('update');
 		expect(Resource).itself.to.respondTo('delete');				
 		expect(Resource).itself.to.respondTo('remove');
+		expect(Resource).itself.to.respondTo('save');	
 
 		var resource = new Resource();
 		expect(resource).to.respondTo('$get');
@@ -25,9 +26,52 @@ describe('Resource', function() {
 		expect(resource).to.respondTo('$update');
 		expect(resource).to.respondTo('$delete');				
 		expect(resource).to.respondTo('$remove');
+		expect(resource).to.respondTo('$save');
 	});
 
-	it('should update when an id is present and create when not when saving', function() {
+	it('should have only GET or HEAD actions when defined as read-only', function() {
+		var extraActions = {
+			post: { method: 'POST' },
+			head: { method: 'HEAD' }
+		};
+
+		var Resource = $resource('/', {}, extraActions, { readOnly: true });
+		expect(Resource).itself.to.respondTo('get');
+		expect(Resource).itself.to.respondTo('query');
+		expect(Resource).itself.to.respondTo('head');
+		expect(Resource).itself.to.not.respondTo('create');
+		expect(Resource).itself.to.not.respondTo('update');
+		expect(Resource).itself.to.not.respondTo('delete');				
+		expect(Resource).itself.to.not.respondTo('remove');
+		expect(Resource).itself.to.not.respondTo('save');	
+		expect(Resource).itself.to.not.respondTo('post');
+
+		var resource = new Resource();
+		expect(resource).to.respondTo('$get');
+		expect(resource).to.respondTo('$query');
+		expect(resource).to.respondTo('$head');
+		expect(resource).to.not.respondTo('$create');
+		expect(resource).to.not.respondTo('$update');
+		expect(resource).to.not.respondTo('$delete');				
+		expect(resource).to.not.respondTo('$remove');
+		expect(resource).to.not.respondTo('$save');
+		expect(resource).to.not.respondTo('$post');
+	});
+
+	it('should create a new cache when options.cache is true, and use a given cache if it is passed in options.cache', inject(function($cacheFactory) {
+		// Part 1: Check that a cache is correctly created
+		const URL = '/shouldbethecachename'
+		var Resource1 = $resource(URL, {}, {}, { cache: true });
+		expect(Resource1.cache).to.exist;
+		expect(Resource1.cache).to.equal($cacheFactory.get(URL));
+
+		// Part 2: Check that a provided cache is used
+		var cache = $cacheFactory('someothercache');
+		Resource2 = $resource('/', {}, {}, { cache: cache });
+		expect(Resource2.cache).to.equal(cache);
+	}));
+
+	it('.save should update when an id is present and create when not', function() {
 		function always() { return true; };
 		$httpBackend
 			.whenPOST(always)
@@ -36,7 +80,7 @@ describe('Resource', function() {
 			.whenPUT(always)
 			.respond(200);
 
-		var Resource = $resource('http://example.com');
+		var Resource = $resource('/');
 
 		var create = sinon.spy(Resource, 'create');
 		var update = sinon.spy(Resource, 'update');
@@ -53,5 +97,26 @@ describe('Resource', function() {
 		update.restore();
 	});
 
-	
+	it('should write the resource to cache when getting', function() {
+		const RESPONSE = { id: 1 };
+		var Resource = $resource('/');
+		$httpBackend
+			.expectGET('/')
+			.respond(200, RESPONSE);
+
+		var cached;
+		var resource1 = Resource.get();
+		expect($httpBackend.flush).to.not.throw;
+
+		var resource2 = Resource.get();
+
+var result
+		expect(result).to.exist.and.have.property('$promise');
+		result.$promise.then(function(x) {
+			cached = Resource.cache.get(RESPONSE.id);
+		});
+		$httpBackend.flush();
+
+		expect(cached).to.exist;
+	})
 });
