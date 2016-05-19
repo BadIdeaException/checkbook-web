0ee369d8fd5c2f2b69d50e624e93b7ab761fc1af
chris <tigerman33@gmx.net>
2016-05-19 19:11:24 +0200

    Changed test for previous renaming of expandURL to expandUrl

--- test/util/expandUrl.spec.js
+++ test/util/expandUrl.spec.js
@@ -1,34 +1,34 @@
-describe('expandURL', function() {
+describe('expandUrl', function() {
 	beforeEach(angular.mock.module('Checkbook.Util'));
 
-	var expandURL;
-	beforeEach(inject(function(_expandURL_) {
-		expandURL = _expandURL_;
+	var expandUrl;
+	beforeEach(inject(function(_expandUrl_) {
+		expandUrl = _expandUrl_;
 	}));
 
 	it('should copy over protocol and domain if they are specified', function() {
 		const URL = 'http://www.example.com/path';
-		var result = expandURL(URL);
+		var result = expandUrl(URL);
 		expect(result).to.equal(URL);
 	});
 
 	it('should copy over query if specified', function() {
 		const URL = '/path?q1=1&q2=2';
-		var result = expandURL(URL);
+		var result = expandUrl(URL);
 		expect(result).to.equal(URL);
 	});
 
 	it('should fill in literals', function() {
 		const URL = '/number/:i/string/:s';
 		var params = { i: 1, s: 'string' };
-		var result = expandURL(URL, params);
+		var result = expandUrl(URL, params);
 		expect(result).to.equal('/number/1/string/string');
 	});
 
 	it('should fill in functions', function() {
 		const URL = '/function/:f';
 		var params = { f: sinon.spy() };
-		var result = expandURL(URL, params);
+		var result = expandUrl(URL, params);
 		expect(params.f).to.have.been.called;		
 	});
 
@@ -36,14 +36,14 @@ describe('expandURL', function() {
 		const URL = '/property/:prop';
 		var params = { prop: '@target' };
 		var data = { target: 'value' };
-		var result = expandURL(URL, params, data);
+		var result = expandUrl(URL, params, data);
 		expect(result).to.equal('/property/' + data.target);
 	});
 
 	it('should append extra parameters in query', function() {
 		const URL = '/path';
 		const PARAMS = { q1: 5, q2: 'x' };
-		var result = expandURL(URL, PARAMS);		
+		var result = expandUrl(URL, PARAMS);		
 		
 		// This test is a little more complicated because we cannot assume 
 		// anything about the order of the query parameters
@@ -61,7 +61,7 @@ describe('expandURL', function() {
 	it('should respect a query that is already present when appending extra parameters', function() {
 		const URL = '/path?p=1';
 		const PARAMS = { q: 'x' };
-		var result = expandURL(URL, PARAMS);
+		var result = expandUrl(URL, PARAMS);
 		expect(result).to.equal(URL + '&q=x');
 	});
 });
