32d6a91aadd3d350a0c257178e1953eedc227c5e
chris <tigerman33@gmx.net>
2016-05-29 14:38:36 +0200

    Adding an element to the Cache also adds it to its collection Removing an element from the Cache also removes it from its collection

--- src/js/data/Cache.js
+++ src/js/data/Cache.js
@@ -7,6 +7,10 @@
 angular
 	.module('Checkbook.Data')
 	.factory('Cache', function() {
+		// Terminology:
+		// An element is a basic thing that can be contained in the cache (for example an object)
+		// A collection can be contained in the cache. It consists of elements (for example an array of objects)
+		// An item can be either an element or a collection
 		var isCollection = angular.isArray;
 
 		function Cache(keygen) {
@@ -30,6 +34,14 @@ angular
 			if (isCollection(item)) {
 				item.forEach(function(item) { self.put(item) });
 			}
+
+			// If we add an element, and there is a collection that this element
+			// should be contained in, add it to the collection as well
+			if (!isCollection(item) && self.has(self.keygen.coll(item))) {
+				var coll = self.get(self.keygen.coll(item));
+				if (coll.indexOf(item) === -1) // THIS WILL ONLY WORK IF ITEMS ARE OBJECTS
+					coll.push(item);
+			}
 		}
 
 		Cache.prototype.get = function(key) {
@@ -39,8 +51,20 @@ angular
 
 		Cache.prototype.remove = function(key) {
 			var self = this;
+			var item = self.items[key];
 			delete self.items[key];
-		}
+
+			if (!isCollection(item) && self.has(self.keygen.coll(item))) {
+				var coll = self.get(self.keygen.coll(item));
+				if (coll.indexOf(item) > -1) // This should always be the case
+					coll.splice(coll.indexOf(item), 1);
+			}
+		};
+
+		Cache.prototype.has = function(key) {
+			var self = this;
+			return self.items[key] !== undefined;
+		};
 
 		return Cache;
 	});
