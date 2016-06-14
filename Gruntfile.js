8e5b22735f27dba113c6ffef842f898e78810009
chris <tigerman33@gmx.net>
2016-06-14 15:19:30 +0200

    Use ngdocs

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jade');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-ngdocs')
 	
 	grunt.registerTask('buildDev', ['jade']);
 }
