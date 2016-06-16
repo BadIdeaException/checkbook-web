4cf2e2bcbd87f0e3ab1497c6e70865ff139ccd82
chris <tigerman33@gmx.net>
2016-06-16 12:56:32 +0200

    Support documentation using dgeni-alive

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jade');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-ngdocs')
-	
+	grunt.loadNpmTasks('dgeni-alive');
+
 	grunt.registerTask('buildDev', ['jade']);
 }
---
+++ grunt/dgeni-alive.grunt.js
@@ -0,0 +1,13 @@
+module.exports = {
+	options: {
+		basePath: '.'
+	},
+
+	checkbook: {
+		title: 'Checkbook',
+		version: '1',
+		expand: true,
+		dest: 'docs',
+		src: ['src/js/**/*.js']
+	}
+}
